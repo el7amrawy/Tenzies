5d9f01aea358b6a7d898932f578b374557b9faad
Aly <el7amrawy@outlook.com>
2022-09-12 22:12:02 +0200

    main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,16 @@
 const Main = () => {
-  return <main>ss</main>;
+  return (
+    <div className="main">
+      <div>
+        <h2>Tenzies</h2>
+        <p>
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+        <div className="nums"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
