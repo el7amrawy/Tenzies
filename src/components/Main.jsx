d85911e77aee8139fb7c9decbc430abdf4c87f0f
Aly <el7amrawy@outlook.com>
2022-09-13 03:12:42 +0200

    random nums

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,33 @@
+import { useState } from "react";
+import Die from "./Die";
+
 const Main = () => {
+  /**
+   * States
+   */
+  const [dice, setDice] = useState(() => allNewDice());
+  //   console.log(dice);
+
+  /**
+   * Functions
+   */
+  function allNewDice() {
+    const arr = [];
+    for (let i = 0; i < 10; i++) {
+      const randomNum = Math.floor(Math.random() * 6) + 1;
+      arr.push(randomNum);
+    }
+    return arr;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+  /**
+   *
+   */
+  const diceElems = dice.map((num) => <Die num={num} />);
+
   return (
     <div className="main">
       <div>
@@ -7,7 +36,8 @@ const Main = () => {
           Roll until all dice are the same. Click each die to freeze it at its
           current value between rolls.
         </p>
-        <div className="nums"></div>
+        <div className="nums">{diceElems}</div>
+        <button onClick={rollDice}>Roll</button>
       </div>
     </div>
   );
